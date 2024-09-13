d54618eb2df3aeba5696f0b910ee4c242bf856d2
brataaaaan <61033560+brataaaan@users.noreply.github.com>
2024-09-13 19:21:34 +0200

    feat: padding for markdown view

--- app/qas/layout.tsx
+++ app/qas/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="container mx-auto my-auto prose prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-4xl prose-h2:text-3xl prose-h3:text-2xl prose-h4:text-xl prose-h5:text-lg prose-h6:text-base prose-a:underline prose-a:underline-offset-4 dark:prose-headings:text-white dark:prose-p:text-white dark:prose-strong:text-white dark:prose-li:text-white dark:prose-code:text-slate-300 dark:prose-a:text-white">
+    <div className="container mx-auto my-auto px-2 prose prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-4xl prose-h2:text-3xl prose-h3:text-2xl prose-h4:text-xl prose-h5:text-lg prose-h6:text-base prose-a:underline prose-a:underline-offset-4 dark:prose-headings:text-white dark:prose-p:text-white dark:prose-strong:text-white dark:prose-li:text-white dark:prose-code:text-slate-300 dark:prose-a:text-white">
       {children}
     </div>
   );
