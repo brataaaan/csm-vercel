f4d3f0e96bb68ad6f6e0569b25b5b1258e6a87e3
brataaaaan <61033560+brataaaan@users.noreply.github.com>
2024-09-13 19:20:10 +0200

    feat: logo invert light

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
         <Image
-          className="light:invert"
+          className="invert dark:invert-0"
           src="https://vercel.com/mktng/_next/static/media/logo-vercel-logotype-dark.e8c0a742.svg"
           alt="Vercel logo"
           width={180}
